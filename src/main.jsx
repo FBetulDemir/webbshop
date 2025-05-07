e2e1ece233910c35b5d3f7baf625f3dbfca99e46
FBetulDemir <fatmabetuld@gmail.com>
2025-05-07 08:30:20 +0200

    Add routing and header components

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createHashRouter, RouterProvider } from 'react-router'
+import Home from './pages/Home.jsx'
+import Products from './pages/Products.jsx'
+
+const router = createHashRouter(
+  [
+    {
+      path: '/',
+      Component: App,
+      children: [
+        {
+          index: true,
+          component: Home
+        },
+        {
+          path: '/pages/products/:productId?',
+          Component: Products,
+        }
+      ]
+    }
+  ]
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/Products.jsx
@@ -0,0 +1,10 @@
+const Products = () => {
+  return (
+    <div>
+      <h1>Products</h1>
+      <p>Here you can find a list of our products.</p>
+    </div>
+  )
+}
+
+export default Products;
