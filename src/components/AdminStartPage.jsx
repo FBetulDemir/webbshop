39b78cdb0e46f42b8417398859c2781b8fe7f96c
FBetulDemir <fatmabetuld@gmail.com>
2025-05-13 08:30:03 +0200

    Added admin start page style

--- src/components/AdminStartPage.jsx
+++ src/components/AdminStartPage.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from 'react-router';
+import '../styles/AdminStartPage.css';
 
 const AdminStartPage = () => {
     const [userNameInput, setUserNameInput] = useState('');
